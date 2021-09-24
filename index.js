780d11de0a9af849e29a653f53c16c71d05fc269
Artem Lamzov <artem.lamzov@motorsport.com>
2021-09-24 17:03:54 +0300

    Добавляет базовую структуру проекта для бэка на Node; Добавляет роуты, схему и базовый код запуска сервера для процедуры регистрации и аутентификации;

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const config = require('config');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = config.get('port') || 5000;
+const MONGOURI = config.get('mongoURI');
+
+app.use('api/auth', require('./routes/auth.routes'));
+
+async function start() {
+    try {
+        await mongoose.connect(MONGOURI, {
+            useUnifiedTopology: true,
+        });
+        app.listen(PORT, () => console.log(`Node works on port: ${PORT}`));
+    } catch (e) {
+        console.log(`Error: ${e.message}`);
+        process.exit(1);
+    }
+}
+
+start();
---
+++ routes/auth.routes.js
@@ -0,0 +1,93 @@
+const { Router } = require('express');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const { check, validationResult } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const router = Router();
+
+// /api/auth/register
+router.post(
+    '/register',
+    [
+        check('email', 'Wrong email format').isEmail,
+        check('password', 'Uncorrect password, minimum 8 symbols').isLength({
+            min: 8,
+        }),
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+
+            if (errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Data uncorrect!',
+                });
+            }
+
+            const { email, password } = req.body;
+            const candidate = await User.findOne({ email });
+
+            if (candidate) {
+                res.status(400).json({ message: 'Username already exists!' });
+            }
+
+            const hashedPassword = await bcrypt.hash(password, 11);
+            const user = new User({ email, password: hashedPassword });
+
+            await user.save();
+
+            res.status(201).json({ message: 'User was create!' });
+        } catch (e) {
+            res.status(500).json({ message: 'Something was wrong...' });
+        }
+    },
+);
+
+// /api/auth/login
+router.post(
+    '/login',
+    [
+        check('email', 'Type correct email').normalizeEmail().isEmail(),
+        check('password', 'Type password').exists(),
+    ],
+    async (req, res) => {
+        try {
+            const errors = validationResult(req);
+
+            if (errors.isEmpty()) {
+                return res.status(400).json({
+                    errors: errors.array(),
+                    message: 'Data uncorrect!',
+                });
+            }
+
+            const { email, password } = req.body;
+
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                return res.status(400).json({ message: "User doesn't exist!" });
+            }
+
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            if (!isMatch) {
+                return res.status(400).json({ message: 'Password incorrect!' });
+            }
+
+            const token = jwt.sign(
+                { userId: user.id },
+                config.get('jwtSecret'),
+                { expiresIn: '1h' },
+            );
+
+            res.json({ token, userId: user.id });
+        } catch (e) {
+            res.status(500).json({ message: 'Something was wrong...' });
+        }
+    },
+);
+
+module.exports = router;
