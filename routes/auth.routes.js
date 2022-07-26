b1d47f122b58aef8d0d47ae8b2fa7de1404b7dc6
Artem <sl163@ya.ru>
2022-07-26 17:32:14 +0300

    Removed old code (headers) from login route;

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -80,22 +80,6 @@ router.post(
         check('password', 'Type password').exists(),
     ],
     async (req, res) => {
-        res.setHeader('Access-Control-Allow-Credentials', true);
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader(
-            'Access-Control-Allow-Methods',
-            'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-        );
-        res.setHeader(
-            'Access-Control-Allow-Headers',
-            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-        );
-
-        if (req.method === 'OPTIONS') {
-            res.status(200).end();
-            return;
-        }
-        
         try {
             const errors = validationResult(req);
 
